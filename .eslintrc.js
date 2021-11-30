119e9c7998c7711776dcc3f9a7a91a802a4cdf47
Đức Huỳnh Thanh <cart.huynh.goldenowl@gmail.com>
2021-11-30 08:57:07 +0700

    set up redux, redux-toolkit, router

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,14 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "prettier", "import"],
+  plugins: ["react", "import", "prettier"],
   rules: {
     "no-console": "warn",
     "import/first": "error",
     "react/prop-types": 0,
     "linebreak-style": ["error", "unix"],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "prettier/prettier": "error",
   },
 }
